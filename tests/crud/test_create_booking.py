2644852377bbde84da97477a412d23790f51d130
Payal Gaikwad <poyal1994@gmail.com>
2026-05-24 23:37:56 +0200

    Initial Commit

---
+++ tests/crud/test_create_booking.py
@@ -0,0 +1,10 @@
+import pytest
+import allure
+
+class TestCreateBooking(object):
+    @pytest.mark.positive
+    @allure.title("Verify that Create Booking Status and Booking ID shouldn't be null")
+    @allure.description(
+        "Creating a Booking from the paylaod and verfiy that booking id should not be null and status code should be 200 for the correct payload")
+    def test_create_booking_positive(self):
+        pass
